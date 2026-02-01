5c2cf2dbee6160cfa417c2734cf08cc23d7eedf9
John Eerkens <johneerkens@hotmail.com>
2026-02-01 22:56:32 +0100

    added new folders, code & fixing issue's

---
+++ lib/core/models/mock_data.dart
@@ -0,0 +1,28 @@
+import 'car_model.dart';
+import 'track_model.dart';
+
+const mockCars = [
+  CarModel(
+    id: 'acc_bmw_m4_gt3',
+    name: 'BMW M4 GT3',
+    simId: 'acc',
+  ),
+  CarModel(
+    id: 'acc_ferrari_296_gt3',
+    name: 'Ferrari 296 GT3',
+    simId: 'acc',
+  ),
+];
+
+const mockTracks = [
+  TrackModel(
+    id: 'monza',
+    name: 'Monza',
+    simId: 'acc',
+  ),
+  TrackModel(
+    id: 'spa',
+    name: 'Spa-Francorchamps',
+    simId: 'acc',
+  ),
+];
---
+++ lib/ui/setup_editor/car_selection_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import '../../core/models/mock_data.dart';
+import '../../core/models/car.dart'; // Make sure this path is correct and points to where Car is defined
+
+class CarSelectionPage extends StatelessWidget {
+  const CarSelectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Select Car')),
+      body: ListView.separated(
+        itemCount: mockCars.length,
+        separatorBuilder: (_, _) => const Divider(height: 1),
+        itemBuilder: (context, i) {
+          final carModel = mockCars[i];
+          final car = Car(
+            name: carModel.name,
+            // Add other fields as necessary to map from CarModel to Car
+          );
+          return ListTile(
+            title: Text(car.name),
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => TrackSelectionPage(car: car),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class TrackSelectionPage extends StatelessWidget {
+  final Car car;
+  const TrackSelectionPage({super.key, required this.car});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Select Track for ${car.name}')),
+      body: Center(child: Text('Track selection goes here')),
+    );
+  }
+}
+
+class Car {
+  final String name;
+  // Add other fields as necessary
+
+  Car({required this.name});
+}
